aa2aafde4ebd1c5a74b76bb764ea0d41ee8a8fb7
ishuar <ishansharma887@gmail.com>
2023-12-22 19:50:30 +0100

    chore: cosmetics on simple example

--- examples/simple/versions.tf
+++ examples/simple/versions.tf
@@ -1,3 +1,9 @@
+/**
+* # Introduction
+*
+* This example show the simplest example of using the module to create a single user-managed identity and assign one custom and built-in role to it respectively.
+*/
+
 provider "azurerm" {
   features {}
 }
