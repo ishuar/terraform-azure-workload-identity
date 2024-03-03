2c3e455a6a617e8a595b897238711bd46f0c3029
ishuar <ishansharma887@gmail.com>
2024-03-03 16:21:32 +0100

    removed unwanted code and switch to old output names
    Signed-off-by: ishuar <ishansharma887@gmail.com>

--- examples/multiple-identities/outputs.tf
+++ examples/multiple-identities/outputs.tf
@@ -1,29 +1,9 @@
-# output "multiple_service_account_identities_client_ids" {
-#   value       = { for k, v in module.multiple_service_account_identities : k => v.client_id }
-#   description = "The Client IDs of the apps associated with the Identities."
-# }
-
-# output "multiple_service_account_identities_subjects" {
-#   value       = { for k, v in module.multiple_service_account_identities : k => v.subject }
-#   description = "The subjects for the Federated Identity Credential associated with the Identities."
-# }
-
-# output "multiple_github_workflow_identities_client_ids" {
-#   value       = { for k, v in module.multiple_github_workflow_identities : k => v.client_id }
-#   description = "The Client IDs of the apps associated with the Identities."
-# }
-
-# output "multiple_github_workflow_identities_subjects" {
-#   value       = { for k, v in module.multiple_github_workflow_identities : k => v.subject }
-#   description = "The subjects for the Federated Identity Credential associated with the Identities."
-# }
-
-output "combination_service_accounts_and_github_workflow_client_ids" {
+output "client_ids" {
   value       = { for k, v in module.combination_service_accounts_and_github_workflow_identities : k => v.client_id }
   description = "The Client IDs of the apps associated with the Identities."
 }
 
-output "combination_service_accounts_and_github_workflow_subjects" {
+output "subjects" {
   value       = { for k, v in module.combination_service_accounts_and_github_workflow_identities : k => v.subject }
   description = "The subjects for the Federated Identity Credential associated with the Identities."
 }
